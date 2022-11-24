109f355d899ef60a402f43ea1e17d3c74aa092e1
Wayne Haworth <waynehaworth@gmail.com>
2022-11-24 21:09:43 +0000

    refactor session

---
+++ Notes-frontend/src/App.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from "react";
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => { 
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  const handleNoteChange = (event) => { setNewNote(event.target.value) }
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(response => {
+        setNotes(response.data)
+      })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    noteService
+      .update(id, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      })
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+  }
+
+  
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={() => setShowAll(!showAll)}>
+        Show {showAll ? 'important' : 'all'}
+      </button>
+      <ul>
+        {notesToShow.map( note =>
+          <Note 
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+          <input
+            value={newNote}
+            onChange={handleNoteChange}
+            />
+          <button type="submit">save</button>
+      </form>      
+    </div>
+  );
+}
+
+export default App;
